030bca15d3ac2f6c8c650f53870823046b09076f
ilaha-jaf <ilash.jafarova@gmail.com>
2024-08-06 12:12:34 +0400

    new

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,21 +4,59 @@ const BasketContext = createContext();
 
 export const BasketProvider = ({ children }) => {
   const [basket, setBasket] = useState([]);
+  const [quantities, setQuantities] = useState({});
 
   const addToBasket = (product) => {
     setBasket((prevBasket) => [...prevBasket, product]);
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [product.id]: (prevQuantities[product.id] || 0) + 1,
+    }));
   };
 
   const removeFromBasket = (productId) => {
     setBasket((prevBasket) => prevBasket.filter(product => product.id !== productId));
+    setQuantities((prevQuantities) => {
+      const { [productId]: _, ...rest } = prevQuantities;
+      return rest;
+    });
+  };
+
+  const incrementQuantity = (productId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: (prevQuantities[productId] || 1) + 1,
+    }));
+  };
+
+  const decrementQuantity = (productId) => {
+    setQuantities((prevQuantities) => {
+      const currentQuantity = prevQuantities[productId] || 1;
+      return {
+        ...prevQuantities,
+        [productId]: Math.max(currentQuantity - 1, 1),
+      };
+    });
   };
 
   const getTotalPrice = () => {
-    return basket.reduce((total, product) => total + parseFloat(product.price), 0);
+    return basket.reduce((total, product) =>
+      total + parseFloat(product.price) * (quantities[product.id] || 1), 0
+    );
   };
 
   return (
-    <BasketContext.Provider value={{ basket, addToBasket, removeFromBasket, getTotalPrice }}>
+    <BasketContext.Provider
+      value={{
+        basket,
+        addToBasket,
+        removeFromBasket,
+        incrementQuantity,
+        decrementQuantity,
+        getTotalPrice,
+        quantities
+      }}
+    >
       {children}
     </BasketContext.Provider>
   );
--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useBasket } from '../Context';
 import "./Basket.css";
 
 const Basket = () => {
-  const { basket, removeFromBasket } = useBasket();
-  const [quantities, setQuantities] = useState({});
-
-  useEffect(() => {
-    setQuantities(basket.reduce((acc, product) => {
-      acc[product.id] = product.quantity || 1;
-      return acc;
-    }, {}));
-  }, [basket]);
-
-  const handleQuantityChange = (productId, change) => {
-    setQuantities(prevQuantities => {
-      const newQuantity = (prevQuantities[productId] || 1) + change;
-      return {
-        ...prevQuantities,
-        [productId]: Math.max(newQuantity, 1),
-      };
-    });
-  };
-
-  const handleRemove = (productId) => {
-    removeFromBasket(productId);
-    setQuantities(prevQuantities => {
-      const { [productId]: _, ...rest } = prevQuantities;
-      return rest;
-    });
-  };
-
-  const getTotalPrice = () => basket.reduce((total, product) =>
-    total + parseFloat(product.price) * (quantities[product.id] || 1), 0
-  );
+  const { basket, removeFromBasket, incrementQuantity, decrementQuantity, getTotalPrice, quantities } = useBasket();
 
   return (
     <div>
@@ -48,11 +18,11 @@ const Basket = () => {
               <img src={image} alt={title} />
               <p>Price: ${price}</p>
               <div className='quantity'>
-                <button onClick={() => handleQuantityChange(id, 1)}>+</button>
-                <p>{quantities[id]}</p>
-                <button onClick={() => handleQuantityChange(id, -1)}>-</button>
+                <button onClick={() => incrementQuantity(id)}>+</button>
+                <p>{quantities?.[id] || 1}</p>
+                <button onClick={() => decrementQuantity(id)}>-</button>
               </div>
-              <button className='delete-btn' onClick={() => handleRemove(id)}>Delete from Basket</button>
+              <button className='delete-btn' onClick={() => removeFromBasket(id)}>Delete from Basket</button>
             </div>
           ))}
           <div className="total-price">
